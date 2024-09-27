05927a27b37d40f5477e219d7c5468a19cbb4c4c
Danillo de Lacerda <danillo.lacerda77@gmail.com>
2024-09-27 19:30:44 -0300

    CoinFlip incompleto

---
+++ CoinFlip/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace CoinFlip
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void FlipButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
