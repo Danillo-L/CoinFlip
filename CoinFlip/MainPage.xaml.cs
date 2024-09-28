9538f0678ee983a5ae4ee45931db3133585c9a66
Danillo de Lacerda <danillo.lacerda77@gmail.com>
2024-09-27 22:38:50 -0300

    Projeto incompleto, falta a imagem

--- CoinFlip/MainPage.xaml.cs
+++ CoinFlip/MainPage.xaml.cs
@@ -2,16 +2,30 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void FlipButton_Clicked(object sender, EventArgs e)
+        Random random = new Random();
+
+        private void JogarButton_Clicked(object sender, EventArgs e)
         {
 
+            Moeda moeda = new Moeda();
+            moeda.SortearLado();
+            string selecao = SelecaoPicker.SelectedItem.ToString().ToLower();
+            if (selecao == moeda.LadoSorteado)
+            {
+                ResultLabel.Text = $"Parabéns, você pediu {selecao} e deu {moeda.LadoSorteado}!";
+
+            } else
+            {
+                ResultLabel.Text = $"Que pena, você pediu {selecao} e deu {moeda.LadoSorteado}!";
+            }
+            MoedaImage.Source = $"{moeda.LadoSorteado}.png";
+         
         }
     }
 
